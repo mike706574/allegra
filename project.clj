90832662cec69e6e270bca9deb6ee427c628bcfb
mike <mike706574@gmail.com>
2016-04-03 16:47:00 -0500

    update deps

--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
   :license {:name "Eclipse Public License 1.0"
             :url "http://opensource.org/licenses/eclipse-1.0.php"
             :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.zip "0.1.1"]
                  [org.clojure/data.csv "0.1.3"]
-                 [clj-http "0.5.8"]
+                 [clj-http "2.1.0"]
                  [com.taoensso/timbre "4.3.1"]]
   :main allegra.core
   :aot :all)
