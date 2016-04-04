94acbc2b8b521b45e47fb27d470a1cac6df789b1
mike <mike706574@gmail.com>
2016-04-03 21:37:47 -0500

    json dep

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.zip "0.1.1"]
                  [org.clojure/data.csv "0.1.3"]
+                 [org.clojure/data.json "0.2.6"]
                  [clj-http "2.1.0"]
                  [com.taoensso/timbre "4.3.1"]]
   :resource-paths ["resources" "test-resources"]
