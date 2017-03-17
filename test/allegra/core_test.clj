4a7ea2fdb22d7b16376d14e94fad547bc954c87d
mike <mike706574@gmail.com>
2017-03-16 21:06:26 -0500

    Comment out test.

--- test/allegra/core_test.clj
+++ test/allegra/core_test.clj
@@ -13,13 +13,14 @@
    ["151902985015" "B001QOGXPK" "EDUCATIONAL INSIGHTS BLURT!" "TOYS_AND_GAMES" "17133" "http://www.amazon.com/gp/product/B001QOGXPK" "http://www.amazon.com/gp/offer-listing/B001QOGXPK%3FSubscriptionId%3DAKIAJFQYE5IY2K6FQK6A%26tag%3Dmike0c7-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D386001%26creativeASIN%3DB001QOGXPK" "12.81" "2.7" "11.8" "6.9" "1.6"]
    ["085955090184" "B00OLGL4M2" "\"Doc McStuffins Kids Rollerskate" " Junior Size 6-12 with Knee Pads\"" "TOYS_AND_GAMES" "246525" "http://www.amazon.com/gp/product/B00OLGL4M2" "0" "50.58" "3.7" "11.4" "9.8" "1.6"]])
 
-(deftest test-run
-  (let [credentials (-> "test-resources/properties.edn"
-                        (slurp)
-                        (edn/read-string)
-                        (:credentials))
-        in "in.txt"
-        out "out.txt"]
-    (run credentials "test-resources/upcs.txt" out)
-    (with-open [rdr (io/reader out)]
-      (is (= expected (mapv #(split % #",") (line-seq rdr)))))))
+(comment
+  (deftest test-run
+    (let [credentials (-> "test-resources/properties.edn"
+                          (slurp)
+                          (edn/read-string)
+                          (:credentials))
+          in "in.txt"
+          out "out.txt"]
+      (run credentials "test-resources/upcs.txt" out)
+      (with-open [rdr (io/reader out)]
+        (is (= expected (mapv #(split % #",") (line-seq rdr))))))))
